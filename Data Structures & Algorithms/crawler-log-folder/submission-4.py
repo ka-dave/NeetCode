80208326d93fdf4d629a88b438519d88fec282a3
Karan Dave <karandave0619@gmail.com>
2026-07-04 23:21:58 -0500

    Add: crawler-log-folder - submission-4

---
+++ Data Structures & Algorithms/crawler-log-folder/submission-4.py
@@ -0,0 +1,15 @@
+import re
+
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        
+        stack = []
+
+        for i in logs:
+            if i == '../':
+                if stack:
+                    stack.pop()
+            elif i != "./":
+                stack.append(i)
+
+        return len(stack)
