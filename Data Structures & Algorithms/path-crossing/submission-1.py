ae9e018f64398508c674d0a4a80aa2eef43a175b
Karan Dave <karandave0619@gmail.com>
2026-08-07 21:30:35 -0500

    Add: path-crossing - submission-1

---
+++ Data Structures & Algorithms/path-crossing/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        x = 0
+        y = 0
+        visited = {(0,0)}
+
+        for i in path:
+            if i == 'N':
+                y+=1
+            if i == 'S':
+                y-=1
+            if i == 'E':
+                x+=1
+            if i == 'W':
+                x-=1
+
+            if (x,y) in visited:
+                return True
+                
+            visited.add((x,y))
+        return False
